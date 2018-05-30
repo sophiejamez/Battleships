67ed0282600b77b74e827c32386ec30221fc6ba3
cweckle <34151362+cweckle@users.noreply.github.com>
2018-05-30 13:56:43 -0700

    english class

--- Game.java
+++ Game.java
@@ -11,6 +11,12 @@ public class Game extends JPanel implements MouseListener,ActionListener
     private boolean placed = false;
     private boolean selected = false;
     private JButton button;
+    private JLabel turn;
+    private String yourTurn;
+    private String compTurn;
+    private String hit;
+    private String miss;
+    private JLabel result;
 
     //constructor - sets the initial conditions for this Game object
     public Game(int width, int height)
@@ -27,13 +33,30 @@ public class Game extends JPanel implements MouseListener,ActionListener
         this.addMouseListener(this);//allows the program to respond to key presses - Don't change
         
         String text = "DONE PLACING YOUR SHIPS?";
-        System.out.println(text);
         
         button = new JButton(text);
         button.setBounds(700,500,200,50);
         this.add(button);
         button.setVisible(true);
         button.addActionListener(this);
+        
+        compTurn = "The computer is guessing now.";
+        yourTurn = "It's your turn to guess!";
+        turn = new JLabel(yourTurn);
+        turn.setForeground(Color.WHITE);
+        turn.setBounds(80,50,800,50);
+        this.add(turn);
+        turn.setVisible(false);
+        turn.setFont(new Font("Courier New", Font.ITALIC, 36));
+        
+        hit = "HIT!";
+        miss = "MISS!";
+        result = new JLabel();
+        result.setForeground(Color.RED);
+        result.setBounds(700, 700, 300, 80);
+        this.add(result);
+        result.setVisible(false);
+        result.setFont(new Font("Courier New", Font.BOLD, 80));
 
         this.setFocusable(true);//I'll tell you later - Don't change
     }
@@ -47,8 +70,13 @@ public class Game extends JPanel implements MouseListener,ActionListener
             {
                 Thread.sleep( 200 );//pause for 200 milliseconds
             }catch( InterruptedException ex ){}
+            try{
+                Thread.sleep(800);
+                result.setVisible(false);
+            }catch(InterruptedException ex){}
 
             this.repaint();//redraw the screen with the updated locations; calls paintComponent below
+            
         }
     }
 
@@ -64,6 +92,7 @@ public class Game extends JPanel implements MouseListener,ActionListener
         }
         else
         {
+            turn.setVisible(true);
             board.drawMini(page);
             player.drawMini(page);
             board.drawGame(page);
@@ -84,7 +113,7 @@ public class Game extends JPanel implements MouseListener,ActionListener
     public void mousePressed(MouseEvent e) {
         int x = e.getX();
         int y = e.getY();
-        System.out.println("Mouse Pressed at X: " + x + " - Y: " + y);
+        result.setVisible(false);
         if(!placed){
             if(!selected){
                 player.act(x,y);
@@ -98,11 +127,28 @@ public class Game extends JPanel implements MouseListener,ActionListener
         else{
             if((computer.bombHit(x, y) == true)) //not sure if this is supposed to be this. We need to drop a bomb, and that bomb is what needs to call bombHit i think
             {
+                result.setForeground(Color.RED);
+                result.setText(hit);
+                result.setVisible(true);
                 board.placeHit(x,y);
             }
-            else
+            else{
+                result.setForeground(new Color(200,200,200));
+                result.setText(miss);
+                result.setVisible(true);
                 board.placeMiss(x,y);
+            }
+            changeText();
         }
+    }
+    
+    
+    
+    public void changeText(){
+        if(turn.getText().equals(yourTurn))
+            turn.setText(compTurn);
+        else
+            turn.setText(yourTurn);
     }
 
     public void mouseReleased(MouseEvent e) {
